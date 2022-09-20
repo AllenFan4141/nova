3b2b124e6dc947c6b0c4555606325a3e2130e8de
AllenFan4141 <yinfan4141@sina.com>
2022-09-20 15:20:00 +0800

    添加前端项目基于Ruoyi-Antdv

--- nova-document/src/main/java/com/kdgcsoft/document/config/DocumentConfiguration.java
+++ nova-document/src/main/java/com/kdgcsoft/document/config/DocumentConfiguration.java
@@ -2,6 +2,7 @@ package com.kdgcsoft.document.config;
 
 import com.kdgcsoft.document.DocumentManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
@@ -11,7 +12,7 @@ import org.springframework.context.annotation.Bean;
  * @author fyin
  * @date 2022年09月20日 11:42
  */
-@EnableAutoConfiguration
+@AutoConfiguration
 @EnableConfigurationProperties(DocumentProperties.class)
 public class DocumentConfiguration {
     @Autowired
